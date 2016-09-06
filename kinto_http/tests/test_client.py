749eb85e35f06a3bf2584ad643fcd75234e0cab2
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-09-06 16:31:18 +0200

    Fix flake8

--- kinto_http/tests/test_client.py
+++ kinto_http/tests/test_client.py
@@ -1,10 +1,8 @@
 import mock
-import pytest
 from six import text_type
 from .support import unittest, mock_response, build_response, get_http_error
 
-from kinto_http import (KintoException, BucketNotFound, Client,
-                        DO_NOT_OVERWRITE)
+from kinto_http import KintoException, BucketNotFound, Client, DO_NOT_OVERWRITE
 from kinto_http.session import create_session
 
 

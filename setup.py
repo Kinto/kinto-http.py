0c4832065e2cc0c91810a7d80a1287a265837569
Mathieu Leplatre <mathieu@mozilla.com>
2017-05-25 10:38:44 +0200

    Update package trove classifiers (fixes #149)

--- setup.py
+++ setup.py
@@ -41,9 +41,9 @@ setup(name='kinto-http',
       license='Apache License (2.0)',
       classifiers=[
           "Programming Language :: Python",
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: Implementation :: CPython",
           "Topic :: Internet :: WWW/HTTP",
           "License :: OSI Approved :: Apache Software License"

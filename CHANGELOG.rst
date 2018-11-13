9826322ba068b5630b80b9413f21a26321172bfa
Rémy HUBSCHER <remy@chefclub.tv>
2018-11-13 08:29:35 +0100

    Back to development: 10.2.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -4,6 +4,12 @@ CHANGELOG
 This document describes changes between each past release.
 
 
+10.2.0 (unreleased)
+===================
+
+- Nothing changed yet.
+
+
 10.1.1 (2018-11-13)
 ===================
 
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ test_requirements = [
 ]
 
 setup(name='kinto-http',
-      version='10.1.1',
+      version='10.2.0.dev0',
       description='Kinto client',
       long_description=README,
       license='Apache License (2.0)',

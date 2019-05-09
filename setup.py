ec76e8de660f11c3c028af88c5a5d912073d91fe
Rémy HUBSCHER <remy@chefclub.tv>
2019-05-09 16:44:39 +0200

    Update classifiers.

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Internet :: WWW/HTTP",
         "License :: OSI Approved :: Apache Software License",

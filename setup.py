6a85f9457348541e92c6d4fd50a2a34408e50de8
Rémy HUBSCHER <remy@chefclub.tv>
2019-05-22 09:54:36 +0200

    Back to development: 10.5.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name="kinto-http",
-    version="10.4.1",
+    version="10.5.0.dev0",
     description="Kinto client",
     long_description=README,
     license="Apache License (2.0)",

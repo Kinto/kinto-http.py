d0ed0e414ae262a877398da84b02ad42ad6b9501
Mathieu Leplatre <mathieu@mozilla.com>
2022-08-22 19:35:24 +0200

    Remove pinned versions in dependencies

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ README = read_file("README.rst")
 CHANGELOG = read_file("CHANGELOG.rst")
 
 INSTALL_REQUIRES = [
-    x.replace(" \\", "")
-    for x in read_file("./requirements.txt").split("\n")
-    if not x.startswith(" ")
+    "requests",
+    "backoff",
+    "unidecode",
 ]
 
 TESTS_REQUIRE = [
